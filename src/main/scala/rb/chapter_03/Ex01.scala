a857f9b22c9697966f8021beed20735e865f5fe4
Argive <yaneeve.shekel@perseus.de>
2018-08-09 11:10:13 +0200

    pattern match list

---
+++ src/main/scala/rb/chapter_03/Ex01.scala
@@ -0,0 +1,15 @@
+package rb.chapter_03
+
+import List._
+
+object Ex01 extends App {
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101
+  }
+
+  assert(x == 3)
+}
