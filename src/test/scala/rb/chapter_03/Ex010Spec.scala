382bdbffe56f2495dd1b2317044dac35e8a36025
Argive <yaneeve.shekel@perseus.de>
2018-08-29 09:20:10 +0200

    foldLeft - test

---
+++ src/test/scala/rb/chapter_03/Ex010Spec.scala
@@ -0,0 +1,32 @@
+package rb.chapter_03
+
+import org.scalacheck.Prop.forAll
+import org.scalacheck.{Arbitrary, Gen, Properties}
+
+import scala.util.{Success, Try}
+import scala.{List => sList}
+
+object Ex010Spec extends Properties("03.10") {
+
+  import Ex10.foldLeft
+
+  private val genf: Gen[(String, Int) => String] = Arbitrary.arbitrary[(String, Int) => String]
+
+  private val list = Gen.containerOf[sList, Int](Gen.chooseNum(Int.MinValue, Int.MaxValue))
+  private val tup = for {
+    l <- list
+    p <- genf
+    str <- Gen.alphaNumStr
+  } yield (p, l, str)
+
+  property("foldLeft") = forAll(tup) { case (p: ((String, Int) => String), l: sList[Int], str: String) =>
+    val real: Try[String] = Try(l.foldLeft(str)(p))
+    val consList: List[Int] = List[Int](l: _*)
+    val ex = Try(foldLeft(consList, str)(p))
+    (real, ex) match {
+      case (Success(r), Success(e)) => r == e
+      case _ => false
+    }
+  }
+
+}
