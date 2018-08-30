c8dd098e22ecfdb5d3f850e3c15add4eb4c1129b
Yaneeve <yaneeve.shekel@gmail.com>
2018-08-30 23:27:53 +0200

    linear(?) flatten (I have to check this when I am less drunk)

--- src/main/scala/rb/chapter_03/Ex15.scala
+++ src/main/scala/rb/chapter_03/Ex15.scala
@@ -2,12 +2,19 @@ package rb.chapter_03
 
 object Ex15 {
 
+  import Ex13.foldRight
   import Ex10.foldLeft
   import Ex14.append
+  import Ex12.reverse
 
-  def flatten[A](lola: List[List[A]]): List[A] = lola match {
-    case Nil => Nil
-    case Cons(h, t) => foldLeft(t, h)(append)
+  def flatten[A](lola: List[List[A]]): List[A] = {
+    lola match {
+      case Nil => Nil
+      case _ =>
+        val rlola = reverse(foldLeft(lola, Nil: List[List[A]]){ case (acc, a) => Cons(reverse(a), acc) })
+        val Cons(h, t) = rlola
+        reverse(foldRight(t, h)(append))
+    }
   }
 
 }
