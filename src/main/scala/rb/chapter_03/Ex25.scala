b63ab5a546f1a64377b37daa083d99daa7039123
Argive <yaneeve.shekel@perseus.de>
2018-11-06 16:57:37 +0100

    due to lack of tests for tree I verified the answers with fpinscala github answers and corrected a few things

--- src/main/scala/rb/chapter_03/Ex25.scala
+++ src/main/scala/rb/chapter_03/Ex25.scala
@@ -5,7 +5,7 @@ object Ex25 {
   def size[A](tree: Tree[A]): Int = {
     tree match {
       case Leaf(_) => 1
-      case Branch(rhs, lhs) => size(rhs) + size(lhs)
+      case Branch(rhs, lhs) => size(rhs) + size(lhs) + 1 // thank you fpinscala answers for correcting me
     }
   }
 
--- src/main/scala/rb/chapter_03/Ex29.scala
+++ src/main/scala/rb/chapter_03/Ex29.scala
@@ -6,15 +6,15 @@ object Ex29 {
     case Leaf(value) => f(value)
     case Branch(left, right) =>
       val foldedLeft = fold(left)(f)(g)
-      val foldedRight = fold(left)(f)(g)
+      val foldedRight = fold(right)(f)(g)
       g(foldedLeft, foldedRight)
   }
 
-  def size[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ + _)
+  def size[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ + _ + 1) // thank you fpinscala answers for correcting me
 
   def maximum(t: Tree[Int]): Int = fold(t)(identity)(_ max _)
 
-  def depth[A](t: Tree[A]): Int = fold(t)(_ => 1)(_ max _)
+  def depth[A](t: Tree[A]): Int = fold(t)(_ => 0)(_ max _ + 1) // thank you fpinscala answers for correcting me
 
   def map[A, B](t: Tree[A])(f: A => B): Tree[B] = fold[A, Tree[B]](t)(value => Leaf(f(value)))(Branch(_, _))
 }
