33d58d5355269071619330f4ca8790cb2a3618fd
Yaneeve <yaneeve.shekel@gmail.com>
2018-11-16 22:38:37 +0100

    after some wine - pun intended

---
+++ src/main/scala/rb/chapter_04/Ex01.scala
@@ -0,0 +1,16 @@
+package rb.chapter_04
+
+object Ex01 {
+
+  def map[A,B](o: Option[A])(f: A => B): Option[B] =
+    o match {
+      case None => None
+      case Some(a) => Some(f(a))
+    }
+
+  def getOrElse[A, B>:A](o: Option[A])(default: => B): B = o match {
+    case None => default
+    case Some(a) => a
+  }
+
+}
