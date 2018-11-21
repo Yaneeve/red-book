e91c664db3a5ec8433c742e2c605431584145822
Argive <yaneeve.shekel@perseus.de>
2018-11-21 09:18:58 +0100

    variance (via emacs :) )

--- src/main/scala/rb/chapter_04/Ex01.scala
+++ src/main/scala/rb/chapter_04/Ex01.scala
@@ -23,4 +23,14 @@ object Ex01 {
     if (f(a)) Some(a) else None
   }) 
 
+  def variance(xs: Seq[Double]): Option[Double] = {
+    xs match {
+      case Nil => None
+      case _ =>
+        val mean = xs.sum / xs.length
+        val squaredDiv = xs.map(x => scala.math.pow(x - mean, 2))
+        Some(squaredDiv.sum / squaredDiv.length)
+    }
+  }
+
 }
